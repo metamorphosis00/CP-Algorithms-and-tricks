8b83da3603fa548d48a4b789d262916b4fa4ed4b
User <acmicpc15@gmail.com>
2019-05-03 12:50:11 +0500

    add reset method

--- Graph/Maximum matching/kuhn_struct.cpp
+++ Graph/Maximum matching/kuhn_struct.cpp
@@ -3,32 +3,35 @@ implementation Kuhn's optimized algorithm with struct
 for find maximum matching in given bipartite graph
 Codeforces problem BerSU Ball https://codeforces.com/contest/489/problem/B
 **/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long
 
 using namespace std;
 
 const int N = 1e5;
 
-int readInt ()
+int readInt()
 {
     bool minus = false;
     int result = 0;
     char ch;
     ch = getchar();
-    while (true)
-    {
-        if (ch == '-') break;
-        if (ch >= '0' && ch <= '9') break;
+    while (true) {
+        if (ch == '-')
+            break;
+        if (ch >= '0' && ch <= '9')
+            break;
         ch = getchar();
     }
-    if (ch == '-') minus = true;
-    else result = ch-'0';
-    while (true)
-    {
+    if (ch == '-')
+        minus = true;
+    else
+        result = ch - '0';
+    while (true) {
         ch = getchar();
-        if (ch < '0' || ch > '9') break;
-        result = result*10 + (ch - '0');
+        if (ch < '0' || ch > '9')
+            break;
+        result = result * 10 + (ch - '0');
     }
     if (minus)
         return -result;
@@ -41,15 +44,34 @@ struct BipartiteMatcher {
     vector<int> mt, tm, used;
     int T = 1;
 
-    BipartiteMatcher(int n, int m) :
-        G(n + 1), mt(m + 1, -1), tm(n + 1, -1), used(n + 1, false) {}
+    BipartiteMatcher(int n, int m)
+        : G(n + 1)
+        , mt(m + 1, -1)
+        , tm(n + 1, -1)
+        , used(n + 1, false)
+    {
+    }
 
-    void addEdge(int a, int b) {
+    void addEdge(int a, int b)
+    {
         G[a].push_back(b);
     }
 
-    bool tryKuhn(int v) {
-        if (used[v] == T) return false;
+    void reset()
+    {
+        T = 1;
+        for (int i = 0; i < G.size(); i++) {
+            G[i].clear();
+        }
+        fill(mt.begin(), mt.end(), -1);
+        fill(tm.begin(), tm.end(), -1);
+        fill(used.begin(), used.end(), false);
+    }
+
+    bool tryKuhn(int v)
+    {
+        if (used[v] == T)
+            return false;
         used[v] = T;
         for (int to : G[v]) {
             if (mt[to] == -1) {
@@ -68,7 +90,8 @@ struct BipartiteMatcher {
         return false;
     }
 
-    int getMaximumMatching() {
+    int getMaximumMatching()
+    {
         int run = 1;
         while (run) {
             run = 0;
@@ -88,12 +111,14 @@ struct BipartiteMatcher {
         return result;
     }
 
-    void solve() {
+    void solve()
+    {
         /// your code here
         cout << getMaximumMatching() << "\n";
     }
 };
-main() {
+main()
+{
     int n = readInt();
     for (int i = 1; i <= n; i++) {
         a[i] = readInt();
