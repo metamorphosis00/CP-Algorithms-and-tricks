d422db9721bdfc4daf3e6479f5b63d1ca2b01a6b
user <acmicpc15@gmail.com>
2020-09-11 12:08:39 +0500

    add segment tree mass update

---
+++ Data structure/seg_tree_group_update2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#define int long long
+
+const int N = 1e5;
+
+using namespace std;
+
+int t[4 * N + 1], col[4 * N + 1];
+bool marked[4 * N + 1];
+void pull(int v)
+{
+    t[v] = max(t[2 * v] * (1 - marked[2 * v]) + col[2 * v], t[2 * v + 1] * (1 - marked[2 * v + 1]) + col[2 * v + 1]);
+}
+void push(int v)
+{
+    if (marked[v]) {
+        col[2 * v] = col[v];
+        col[2 * v + 1] = col[v];
+        col[v] = 0;
+
+        marked[2 * v] = true;
+        marked[2 * v + 1] = true;
+        marked[v] = false;
+    }
+}
+void update(int v, int tl, int tr, int L, int R, int x)
+{
+    if (L > R)
+        return;
+    if (tl == L && tr == R) {
+        col[v] = x;
+        marked[v] = true;
+        return;
+    }
+    push(v);
+
+    int tm = (tl + tr) >> 1;
+
+    update(2 * v, tl, tm, L, min(tm, R), x);
+    update(2 * v + 1, tm + 1, tr, max(tm + 1, L), R, x);
+
+    pull(v);
+}
+int getMax(int v, int tl, int tr, int L, int R)
+{
+    if (L > R) {
+        return 0;
+    }
+    if (tl == L && tr == R) {
+        return t[v] * (1 - marked[v]) + col[v];
+    }
+    push(v);
+
+    int tm = (tl + tr) >> 1;
+    int ans = 0;
+    ans = max(getMax(2 * v, tl, tm, L, min(tm, R)), getMax(2 * v + 1, tm + 1, tr, max(L, tm + 1), R));
+    pull(v);
+    return ans;
+}
+main()
+{
+}
